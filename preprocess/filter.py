17e760048d1fcf85d5a87a4d972772117c703474
wendyqiu <wendyqiu1995@yahoo.com>
2021-03-23 23:13:28 -0400

    preprocess COHA (select based on keywords)

--- preprocess/filter.py
+++ preprocess/filter.py
@@ -4,7 +4,13 @@ for efficiency, select relevant data from COHA
 and use these samples in the domain-specific pre-training stage
 """
 import os
+import re
 from nltk import tokenize
+
+# decade/directory number (from 1 to 10)
+NUM = 10
+current_decade = '19' + str(NUM) + '0'
+CHAR_SET = 'utf-8'
 
 # 16 words in the human similarity dataset (PWHS: pairwise human similarity)
 PWHS = ["federal", "spine", "optical", "compact", "signal", "leaf", "net", "coach", "sphere", "mirror",
@@ -18,22 +24,46 @@ GEMS = ["environmental", "users", "virtual", "disk", "tenure", "coach", "address
 filter_keywords = PWHS + GEMS
 
 CORPUS_DIR = 'C:/Users/Mizuk/Documents/phD/csc2611/COHA/raw/'
+OUT_DIR = 'C:/Users/Mizuk/Documents/phD/csc2611/COHA/filtered/'
 
 # read in files
 sub_dir_list = []
 for dirpath, dirnames, files in os.walk(CORPUS_DIR):
     sub_dir_list.append(dirpath)
 
-# todo: modify this
-current_dir = sub_dir_list[1]
+current_dir = sub_dir_list[NUM]
+current_out_dir = os.path.join(OUT_DIR, current_decade)
 print("current directory: {}".format(current_dir))
+print("output directory: {}".format(current_out_dir))
+
+all_counter = 0
+use_counter = 0
 for file_name in os.listdir(current_dir):
     file_path = os.path.join(current_dir, file_name)
     if file_path.endswith(".txt"):
+        all_counter += 1
         print("processing {}".format(file_path))
-        with open(file_path, 'r', encoding='utf-8') as fr:
+        # input("correct?")
+        with open(file_path, 'r', encoding=CHAR_SET) as fr:
             text = fr.read()
             # check filter keywords
-            for sentence in tokenize.sent_tokenize(text):
-                # write to new file
-                pass
+            save = False
+            for search_word in filter_keywords:
+                if save:
+                    break
+                elif search_word in text:
+                    print("search_word is {}".format(search_word))
+                    save = True
+            if save:
+                use_counter += 1
+                # write to a new file
+                output_path = os.path.join(current_out_dir, file_name)
+                print("saving to : {}".format(output_path))
+                with open(output_path, 'w', encoding=CHAR_SET) as fw:
+                    # write sentence in lines
+                    for sent in tokenize.sent_tokenize(text):
+                        sent += "\n"
+                        fw.write(sent)
+
+print("total number of files processed in {0}s: {1}".format(current_decade, all_counter))
+print("number of usable files after filtering: {}".format(use_counter))
